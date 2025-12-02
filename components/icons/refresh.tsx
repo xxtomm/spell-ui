72ceee8381f84408b4c696acd30bbf2787663dbe
xxtomm <3xteam.dev@gmail.com>
2025-12-03 06:57:59 +0900

    fix: change refresh icon

--- components/icons/refresh.tsx
+++ components/icons/refresh.tsx
@@ -1,24 +1,32 @@
-import React, {SVGProps} from 'react';
+import React, { SVGProps } from 'react';
 
 type IconProps = SVGProps<SVGSVGElement> & {
-	secondaryfill?: string;
-	strokewidth?: number;
-	title?: string;
-}
-
-function RefreshAnticlockwise({fill = 'currentColor', secondaryfill, ...props}: IconProps) {
-	secondaryfill = secondaryfill || fill;
-
-	return (
-		<svg height="20" width="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
-	<g fill={fill}>
-		<polygon fill={fill} points="4.367 3.044 3.771 6.798 7.516 6.145 4.367 3.044" stroke={fill} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
-		<polygon fill={secondaryfill} points="15.633 16.956 16.229 13.202 12.484 13.855 15.633 16.956" stroke={secondaryfill} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
-		<path d="m5.101,5c1.263-1.237,2.992-2,4.899-2,3.866,0,7,3.134,7,7" fill="none" stroke={fill} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
-		<path d="m14.899,15c-1.263,1.237-2.992,2-4.899,2-3.866,0-7-3.134-7-7" fill="none" stroke={secondaryfill} strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
-	</g>
-</svg>
-	);
+  secondaryfill?: string;
+  strokewidth?: number;
+  title?: string;
 };
+
+function RefreshAnticlockwise({
+  fill = 'currentColor',
+  strokewidth = 2,
+  ...props
+}: IconProps) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M5 4V7C5 7.55228 5.44772 8 6 8H8.75M19.0118 20V17C19.0118 16.4477 18.5641 16 18.0118 16H15.0118M4 12C4 16.4183 7.58172 20 12 20C14.6362 20 17.0303 18.7249 18.5 16.7578M20 12C20 7.58172 16.4183 4 12 4C9.36378 4 6.96969 5.27512 5.5 7.24224"
+        stroke={fill}
+        strokeWidth={strokewidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
 
 export default RefreshAnticlockwise;
