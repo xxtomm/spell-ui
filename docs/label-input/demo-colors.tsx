21625ee6d76defec191d5d5b9480e20cdaef3e88
xxtomm <3xteam.dev@gmail.com>
2026-01-14 15:02:34 +0900

    fix: fix label input doc

--- docs/label-input/demo-colors.tsx
+++ docs/label-input/demo-colors.tsx
@@ -2,11 +2,10 @@ import { LabelInput } from "@/registry/spell-ui/label-input";
 
 export function Demo() {
   return (
-    <div className="flex gap-4">
+    <div className="flex not-md:flex-col gap-4">
       <LabelInput label="Primary" ringColor="primary" className="max-w-40" />
       <LabelInput label="Orange" ringColor="orange" className="max-w-40" />
       <LabelInput label="Blue" ringColor="blue" className="max-w-40" />
-      <LabelInput label="Green" ringColor="green" className="max-w-40" />
     </div>
   );
 }
